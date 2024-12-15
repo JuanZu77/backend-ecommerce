3f48d0a3574fcc7c75eb493953ca136324a594d7
JuanZu77 <juanvzubiri77@gmail.com>
2024-12-14 21:30:00 -0300

    first commit

---
+++ src/test/java/com/juanzubiri/ecommerce/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanzubiri.ecommerce.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
