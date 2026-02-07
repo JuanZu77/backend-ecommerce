48b37b0b8bd6a667fdd2ef7b0bf5cab9e5bb0b23
JuanZu77 <juan_zubiri@hotmail.com>
2026-02-07 13:30:40 -0300

    feat(order): add order and user endpoints

--- src/main/java/com/juanzubiri/ecommerce/backend/infrastructure/rest/OrderController.java
+++ src/main/java/com/juanzubiri/ecommerce/backend/infrastructure/rest/OrderController.java
@@ -1,6 +1,7 @@
 	package com.juanzubiri.ecommerce.backend.infrastructure.rest;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("api/v1/orders")
 @Slf4j
+@CrossOrigin(origins = "http://localhost:4200")
 public class OrderController {
 
 	private final OrderService orderService;
--- src/main/java/com/juanzubiri/ecommerce/backend/infrastructure/rest/UserController.java
+++ src/main/java/com/juanzubiri/ecommerce/backend/infrastructure/rest/UserController.java
@@ -2,6 +2,7 @@ package com.juanzubiri.ecommerce.backend.infrastructure.rest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import com.juanzubiri.ecommerce.backend.domain.model.User;
 
 @RestController
 @RequestMapping("/api/v1/users") //http://localhost:8085/api/v1/users
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 	
 	private final UserService userService;
